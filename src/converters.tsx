51346c00e76c0b1cb8c7ad082e2fab87370504c7
Aarni Koskela <akx@iki.fi>
2019-11-21 17:27:39 +0200

    Add scsv

--- src/converters.tsx
+++ src/converters.tsx
@@ -1,16 +1,21 @@
-import * as d3dsv from "d3-dsv";
+import { dsvFormat } from "d3-dsv";
 import toml from "toml";
 import yaml from "js-yaml";
 import { DestinationConverter, SourceConverter, StringTransformResult } from "./types";
 import { tableConverter } from "./TableView";
 import { xlsxConverter } from "./XlsxView";
 
+const csv = dsvFormat(",");
+const scsv = dsvFormat(";");
+const tsv = dsvFormat("\t");
+
 export const sourceConverters: { [key: string]: SourceConverter } = {
-  csv: d3dsv.csvParse,
+  csv: csv.parse,
+  scsv: scsv.parse,
   json: JSON.parse,
   text: data => data,
   toml: toml.parse,
-  tsv: d3dsv.tsvParse,
+  tsv: tsv.parse,
   yaml: yaml.safeLoad,
 };
 
@@ -19,10 +24,11 @@ const stringTransform = (fn: (data: any) => string) => (data: any) =>
 
 export const destinationConverters: { [key: string]: DestinationConverter } = {
   "json-compact": stringTransform(JSON.stringify),
-  csv: stringTransform(d3dsv.csvFormat),
+  csv: stringTransform(csv.format),
+  scsv: stringTransform(scsv.format),
   json: stringTransform(data => JSON.stringify(data, null, 2)),
   text: stringTransform(data => "" + data),
-  tsv: stringTransform(d3dsv.tsvFormat),
+  tsv: stringTransform(tsv.format),
   yaml: stringTransform(yaml.safeDump),
   table: tableConverter,
   xlsx: xlsxConverter,
