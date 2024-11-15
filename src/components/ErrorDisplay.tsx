da76c909ba3ed63a3b820654356aff15e9e5d1ad
Aarni Koskela <akx@iki.fi>
2024-11-15 10:41:20 +0200

    Display error messages, if any

--- src/components/ErrorDisplay.tsx
+++ src/components/ErrorDisplay.tsx
@@ -6,6 +6,7 @@ export const ErrorDisplay = ({ result: { error, phase } }: { result: ErrorTransf
     <h2>
       {error.name || "Error"} in {phase}
     </h2>
+    {error.message && <div className="error-message">{error.message}</div>}
     {error.stack ? <div className="error-stack">{error.stack}</div> : <b>{error.toString()}</b>}
   </div>
 );
