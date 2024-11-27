fa86875d440f73656181d1fae97ab5542a5c2d06
Aarni Koskela <akx@iki.fi>
2024-11-27 09:16:46 +0200

    Upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: { "react-refresh": reactRefresh },
+    rules: { "react-refresh/only-export-components": "warn" },
+  },
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: reactHooks.configs.recommended.rules,
+  },
+  {
+    files: ["samples/*.js"],
+    languageOptions: { globals: { data: "writable" } },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
+    },
+  },
+  eslintConfigPrettier,
+];
